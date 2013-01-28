0ed0e4f55c337e77d700acc6f36249394a5f0a45
richardemslie <richard@clue.co.za>
2013-01-28 21:23:33 +0200

    bg thread updating status bar

--- sublime-plugin/Eg/Eg.py
+++ sublime-plugin/Eg/Eg.py
@@ -1,10 +1,44 @@
 import sublime
 import sublime_plugin
+import threading
 
 class EgCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
 
         self.view.set_status('Eg', 'plugin RAN')
+        thread = EgApiCall()
+        thread.start()
+        self.thread_result(thread)
+
+    def thread_result(self, thread):
+
+        #
+        # recurse a lambda to continuously check for status result
+        #
+
+        sublime.set_timeout(lambda: self.thread_result(thread), 10)
+        self.view.set_status('Eg', str(thread.result))
 
 
+class EgApiCall(threading.Thread):
+    
+    def __init__(self):
+
+        threading.Thread.__init__(self)
+        self.result = 0
+
+    def loop(self, count):
+
+        #
+        # loops, incrementing count every 100th of a second
+        # and write into the status bar
+        # 
+
+        count += 1
+        self.result = 'number %i into the status bar' % count
+        sublime.set_timeout(lambda: self.loop(count), 10)
+
+    def run(self):
+
+        self.loop(1)
--- sublime-plugin/README.md
+++ sublime-plugin/README.md
@@ -4,6 +4,6 @@
 * open / (or create) edit pane by name
 * write to specific line (replace)
 * detect text highlight/selection selection (whole word)
-* continuous bg thread with callback/polled for has_update
+* DONE continuous bg thread with callback/polled for has_update
 * install script
 * DONE bind to menu / key_combo to switch plugin on/off 
